7c362ab1c107017459a0ecb2e04bf1479adb0db4
karinak2001 <karinak2001@edu.aac.ac.il>
2024-04-23 16:17:39 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a number: ");
+        int n = scanner.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            System.out.print(i + ", ");
+
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+}
